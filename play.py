e657dbc892175d3fc2e93d46937174f7190e7618
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-09-04 17:13:03 -0500

    add comments, fix end status

--- play.py
+++ play.py
@@ -1,14 +1,18 @@
-from TicTacToe import TicTacToe
+from TicTacToe import Board
 
-game = TicTacToe()
+# initializing the game and setting status
+game = Board()
 game_status = (False, None)
 
+# for assigning first and second move
 p1 = None
 p2 = None
 
+# tracks number of moves made to ensure plays are made in order
 p1_ct = 0
 p2_ct = 0
 
+# setup loop
 while p1 is None:
     tmp = input("\nPlease enter which player goes first: ")
     tmp = tmp.upper()
@@ -24,6 +28,7 @@ while p1 is None:
 
     print("\n")
 
+# play loop
 while not game_status[0]:
     if p1_ct <= p2_ct:
         player = p1
@@ -50,7 +55,13 @@ while not game_status[0]:
 
     game_status = game.board_status()
 
-
-print(
-    "\n" + game.string_board() + "\n\n" + f"Congratulation '{game_status[1]}', you won!"
-)
+# completion and results
+if game_status[1] != None:
+    print(
+        "\n"
+        + game.string_board()
+        + "\n\n"
+        + f"Congratulation '{game_status[1]}', you won!"
+    )
+else:
+    print("\n" + game.string_board() + "\n\n" + "Draw!")
