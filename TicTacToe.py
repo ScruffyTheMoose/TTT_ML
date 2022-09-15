9f081e1a7d9d17291506722586ad59bf44797682
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-09-14 20:25:45 -0500

    major refactor, create GameTools class

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Board:
     def __init__(self) -> None:
         self.board = [
@@ -5,6 +8,18 @@ class Board:
             [-1, -1, -1],
             [-1, -1, -1],
         ]
+
+    def get_board(self) -> list[list()]:
+        """Getter for the game board object
+
+        Args:
+            self (_type_): class method
+
+        Returns:
+            matrix: TTT game board
+        """
+
+        return self.board
 
     def move(self, row: int, column: int, player: str) -> bool:
         """Places a new piece on the board
@@ -27,7 +42,45 @@ class Board:
 
         return False
 
-    def avail_moves(self) -> list(tuple()):
+    def print(self) -> str:
+        """Generates a string version of the game board for printing to console
+
+        Returns:
+            str: a multi-line string representation of the game board
+        """
+
+        # initial string
+        str_board = ""
+
+        # iterating through the rows
+        for i, row in enumerate(self.board):
+            # iterating through the elements in each row
+            for j, element in enumerate(row):
+
+                # -1 is an open space
+                if element == -1:
+                    str_board += "   "
+                # 0 is an O
+                elif element == 0:
+                    str_board += " O "
+                # if not O or empty, then X
+                else:
+                    str_board += " X "
+
+                if j != 2:
+                    str_board += "|"
+
+            if i != 2:
+                str_board += "\n-----------\n"
+
+        return str_board
+
+
+# a separate class to hold static methods strictly for making observations about any game board
+# probably not necessary, but nice for organization at the moment
+class GameTools:
+    @staticmethod
+    def avail_moves(board: list[list]) -> list[tuple]:
         """Returns a list of ordered pairs references available spaces on the board
 
         Args:
@@ -40,7 +93,7 @@ class Board:
         result = list()
 
         # iterate through rows
-        for i, row in enumerate(self.board):
+        for i, row in enumerate(board):
             # iterate through elements in row
             for j, element in enumerate(row):
 
@@ -51,7 +104,8 @@ class Board:
 
         return result
 
-    def board_status(self) -> tuple:
+    @staticmethod
+    def board_status(board: list[list]) -> tuple:
         """Checks the board for 4 conditions:
         - Game ongoing
         - Game draw
@@ -62,135 +116,124 @@ class Board:
             tuple: (<status>, <winner>)
         """
 
+        def check_rows() -> tuple:
+            """Checks the rows of the game board for a winning position
+
+            Returns:
+                tuple: (<status>, <winner>)
+            """
+
+            for row in board:
+                # checking for False values in row to bypass summation
+                if -1 in row:
+                    continue
+
+                # checking the row for status
+                check = check_triple(row)
+
+                if check[0]:
+                    return check
+
+            # if no winning rows found, return False
+            return (False, None)
+
+        def check_columns() -> tuple:
+            """Checks the columns of the game board for a winning position
+
+            Returns:
+                tuple: (<status>, <winner>)
+            """
+
+            for column in range(3):
+                # building column into list
+                col = [x[column] for x in board]
+
+                # checking for False values in column to bypass summation
+                if -1 in col:
+                    continue
+
+                # checking column for status
+                check = check_triple(col)
+
+                if check[0]:
+                    return check
+
+            # if no winning columns found, return False
+            return (False, None)
+
+        def check_diag() -> tuple:
+            """Checks the diagonals of the game board for a winning position
+
+            Returns:
+                tuple: (<status>, <winner>)
+            """
+
+            # generating diagonals of game board into two lists
+            diag_left = [board[i][i] for i in range(3)]
+            diag_right = [board[-i][i - 1] for i in range(1, 4)]
+
+            # checking left status
+            stat = check_triple(diag_left)
+            if stat[0]:
+                return stat
+
+            # checking right status
+            stat = check_triple(diag_right)
+            if stat[0]:
+                return stat
+
+            return (False, None)
+
+        def check_triple(triple: list) -> tuple:
+            """Checks a list of 3 elements for a winning position
+
+            Args:
+                triple (list): list of 3 adjacent elements from the game board
+
+            Returns:
+                tuple: (<status>, <winner>)
+            """
+
+            # if there is an open space in the given list, then there is no winner
+            if -1 in triple:
+                return (False, None)
+
+            # the sum of the elements in the row will indicate status
+            ct = sum(triple)
+            if ct == 0:
+                return (True, "O")
+            elif ct == 3:
+                return (True, "X")
+            else:
+                return (False, None)
+
+        def check_draw() -> tuple:
+            """Checks the game board for a draw
+
+            Returns:
+                tuple: (<status>, <winner>)
+            """
+
+            for row in board:
+                if -1 in row:
+                    return (False, None)
+
+            return (True, None)
+
         # running the three checks and storing outputs to variables
-        r = self._check_rows()
-        c = self._check_columns()
-        d = self._check_diag()
+        r = check_rows()
+        c = check_columns()
+        d = check_diag()
 
         # checking the outputs for a winning case
         for t in [r, c, d]:
             if True in t:
                 return t
 
-        return self._check_draw()
+        return check_draw()
 
-    def _check_rows(self) -> tuple:
-        """Checks the rows of the game board for a winning position
-
-        Returns:
-            tuple: (<status>, <winner>)
-        """
-
-        for row in self.board:
-            # checking for False values in row to bypass summation
-            if -1 in row:
-                continue
-
-            # checking the row for status
-            check = self._check_triple(row)
-
-            if check[0]:
-                return check
-
-        # if no winning rows found, return False
-        return (False, None)
-
-    def _check_columns(self) -> tuple:
-        """Checks the columns of the game board for a winning position
-
-        Returns:
-            tuple: (<status>, <winner>)
-        """
-
-        for column in range(3):
-            # building column into list
-            col = [x[column] for x in self.board]
-
-            # checking for False values in column to bypass summation
-            if -1 in col:
-                continue
-
-            # checking column for status
-            check = self._check_triple(col)
-
-            if check[0]:
-                return check
-
-        # if no winning columns found, return False
-        return (False, None)
-
-    def _check_diag(self) -> tuple:
-        """Checks the diagonals of the game board for a winning position
-
-        Returns:
-            tuple: (<status>, <winner>)
-        """
-
-        # generating diagonals of game board into two lists
-        diag_left = [self.board[i][i] for i in range(3)]
-        diag_right = [self.board[-i][i - 1] for i in range(1, 4)]
-
-        # checking left status
-        stat = self._check_triple(diag_left)
-        if stat[0]:
-            return stat
-
-        # checking right status
-        stat = self._check_triple(diag_right)
-        if stat[0]:
-            return stat
-
-        return (False, None)
-
-    def _check_triple(self, triple: list) -> tuple:
-        """Checks a list of 3 elements for a winning position
-
-        Args:
-            triple (list): list of 3 adjacent elements from the game board
-
-        Returns:
-            tuple: (<status>, <winner>)
-        """
-
-        # if there is an open space in the given list, then there is no winner
-        if -1 in triple:
-            return (False, None)
-
-        # the sum of the elements in the row will indicate status
-        ct = sum(triple)
-        if ct == 0:
-            return (True, "O")
-        elif ct == 3:
-            return (True, "X")
-        else:
-            return (False, None)
-
-    def _check_draw(self) -> tuple:
-        """Checks the game board for a draw
-
-        Returns:
-            tuple: (<status>, <winner>)
-        """
-
-        for row in self.board:
-            if -1 in row:
-                return (False, None)
-
-        return (True, None)
-
-    def get_board(self) -> list(list()):
-        """Getter for the game board object
-
-        Args:
-            self (_type_): class method
-
-        Returns:
-            matrix: list(list())
-        """
-
-        return self.board
-
-    def string_board(self) -> str:
+    @staticmethod
+    def string_board() -> str:
         """Generates a string version of the game board for printing to console
 
         Returns:
@@ -201,7 +244,7 @@ class Board:
         board = ""
 
         # iterating through the rows
-        for i, row in enumerate(self.board):
+        for i, row in enumerate(board):
             # iterating through the elements in each row
             for j, element in enumerate(row):
 
@@ -220,5 +263,54 @@ class Board:
 
             if i != 2:
                 board += "\n-----------\n"
+
+        return board
+
+    @staticmethod
+    def random_board(first_player: int, open_pos: int) -> list[list]:
+        """Generates random TTT game board
+
+        Args:
+            first_player (int): player making first move; 'X' = 1, 'O' = 0
+            open_spaces (int): number of open positions to leave on the board
+
+        Returns:
+            list[list]: a playable TicTacToe game board
+        """
+
+        board = [[-1, -1, -1] for _ in range(3)]
+
+        # generating list of all positions on the board to randomly choose from
+        # this is better than the alternative of randomly generating numbers
+        coords = []
+        for r in range(3):
+            for c in range(3):
+                coords.append((r, c))
+
+        # placing moves on the board until desired number of positions filled
+        pos = 9
+        while pos > open_pos:
+
+            # choosing a random position to take
+            move = random.choice(coords)
+            coords.remove(move)
+
+            if pos % 2 != 0:
+                board[move[0]][move[1]] = first_player
+            else:
+                board[move[0]][move[1]] = 1 - first_player
+
+            # need to check the game status each iteration
+            # if we return build a completed match, we want to know
+
+            pos -= 1
+
+        # dict to return containing all relevant information from the randomized match
+        results = {
+            "board": board,
+            "move_history": None,  # ordered list of who moved and where
+            "game_status": None,  # ongoing or over
+            "winner": None,  # None, X, or O
+        }
 
         return board
