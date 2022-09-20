356f6e747ebbb3086b8ef1934043421d38a5e323
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-09-19 20:08:24 -0500

    simplify sample generator and add comments

--- data/sample.py
+++ data/sample.py
@@ -8,67 +8,57 @@ def random_samples(
     directory: str = "samples.csv",
     rand_first_player: bool = True,
     first_player: int = 1,
-    fixed_open_spaces: bool = False,
     open_space_range: tuple = (1, 8),
     shuffled: bool = True,
-) -> None:
+) -> pd.DataFrame:
     """Creates a set of random samples and saves them to the specified directory as a .csv file.
+
+    n_samples: the number of samples to generate
+    directory: the directory and filename
+    rand_first_player: will randomly select first player for each sample
+    first_player: if not rand_first_player, this will be the first player for each sample
+    open_space_range: the lower and upper bound for randomly determining the number of open spaces to leave on the board
+    shuffled: will shuffle the samples into a random order
 
     Returns:
         File: a CSV file containing the sample data
     """
 
+    # shorter variable name to be called later
     osr = open_space_range
 
+    # list to store the samples that we generate
     samples = list()
 
-    if rand_first_player and not fixed_open_spaces:
-
+    # Condition 1
+    if rand_first_player:
+        # generate n samples
         for _ in range(n_samples):
-
-            player = random.randint(0, 1)
-            open_spaces = random.randint(1, 8)
-
-            match = gt.randomized_match(first_player=player, open_pos=open_spaces)
-
-            samples.append(match)
-
-    elif rand_first_player and fixed_open_spaces:
-
-        for _ in range(n_samples):
-
+            # determining player and open space count
             player = random.randint(0, 1)
             open_spaces = random.randint(osr[0], osr[1])
-
+            # generating and storing the sample
             match = gt.randomized_match(first_player=player, open_pos=open_spaces)
-
             samples.append(match)
 
-    elif not rand_first_player and not fixed_open_spaces:
-
-        for _ in range(n_samples):
-
-            player = first_player
-            open_spaces = random.randint(1, 8)
-
-            match = gt.randomized_match(first_player=player, open_pos=open_spaces)
-
-            samples.append(match)
-
+    # Condition 2
     else:
-
+        # generating n samples
         for _ in range(n_samples):
-
+            # setting player and determining open space count
             player = first_player
             open_spaces = random.randint(osr[0], osr[1])
-
+            # generating and storing the sample
             match = gt.randomized_match(first_player=player, open_pos=open_spaces)
-
             samples.append(match)
 
+    # will randomly order the samples before saving
     if shuffled:
         samples = random.shuffle(samples)
 
+    # converting the list into a DataFrame and then a .csv file
     df = pd.DataFrame(samples)
-
     df.to_csv(path_or_buf=directory, index=False)
+
+    # returning the DataFrame
+    return df
